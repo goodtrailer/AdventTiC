30949f98eba4905da432c0bede6259f875a0ed90
goodtrailer <aldenwu0@gmail.com>
2021-12-26 01:06:01 -0800

    Implement material stats for handle parts

--- src/main/java/goodtrailer/adventtic/materials/AdventTiCMaterialStatsProvider.java
+++ src/main/java/goodtrailer/adventtic/materials/AdventTiCMaterialStatsProvider.java
@@ -34,40 +34,40 @@ public class AdventTiCMaterialStatsProvider extends AbstractMaterialStatsDataPro
     private void addGeneralMaterials()
     {
         addMaterialStats(AdventTiCMaterials.BLAZIUM,
-                new HandleMaterialStats(0.7f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.70f, 0.85f, 0.90f, 1.10f),
                 new HeadMaterialStats(800, 4.5f, 4, 3.6f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.EMBERSTONE,
-                new HandleMaterialStats(0.65f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.65f, 0.80f, 1.00f, 1.10f),
                 new HeadMaterialStats(640, 4.0f, 5, 3.8f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.GHOULISH,
-                new HandleMaterialStats(1.3f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(1.30f, 0.80f, 1.00f, 1.05f),
                 new HeadMaterialStats(1000, 3.9f, 6, 3.2f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.LIMONITE,
-                new HandleMaterialStats(0.6f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.60f, 0.75f, 1.00f, 0.95f),
                 new HeadMaterialStats(250, 3.0f, 2, 2.8f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.LYON,
-                new HandleMaterialStats(1.05f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(1.05f, 1.05f, 1.10f, 1.15f),
                 new HeadMaterialStats(840, 6.2f, 6, 4.0f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.ROSITE,
-                new HandleMaterialStats(0.95f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.95f, 0.75f, 1.00f, 1.05f),
                 new HeadMaterialStats(400, 3.4f, 3, 3.4f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.SHYRESTONE,
-                new HandleMaterialStats(1.0f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(1.00f, 1.40f, 1.10f, 1.25f),
                 new HeadMaterialStats(640, 13.0f, 6, 3.6f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.SKELETAL,
-                new HandleMaterialStats(0.75f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.65f, 1.25f, 1.00f, 1.10f),
                 new HeadMaterialStats(610, 10.0f, 5, 4.3f),
                 ExtraMaterialStats.DEFAULT);
 
         addMaterialStats(AdventTiCMaterials.CHARGER,
-                new HandleMaterialStats(0.5f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.50f, 0.90f, 0.90f, 0.50f),
                 new HeadMaterialStats(110, 2.4f, 0, 2.5f),
                 ExtraMaterialStats.DEFAULT);
     }
@@ -75,7 +75,7 @@ public class AdventTiCMaterialStatsProvider extends AbstractMaterialStatsDataPro
     private void addHarvestMaterials()
     {
         addMaterialStats(AdventTiCMaterials.LUNAR,
-                new HandleMaterialStats(0.9f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.90f, 1.10f, 1.00f, 1.00f),
                 new HeadMaterialStats(660, 3.8f, 6, 2.0f),
                 ExtraMaterialStats.DEFAULT);
     }
@@ -83,23 +83,23 @@ public class AdventTiCMaterialStatsProvider extends AbstractMaterialStatsDataPro
     private void addWeaponMaterials()
     {
         addMaterialStats(AdventTiCMaterials.BARONYTE,
-                new HandleMaterialStats(1.15f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(1.15f, 0.95f, 1.00f, 1.10f),
                 new HeadMaterialStats(900, 5.3f, 4, 3.75f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.ELECANIUM,
-                new HandleMaterialStats(2.0f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(2.00f, 1.15f, 0.90f, 1.40f),
                 new HeadMaterialStats(600, 8.3f, 6, 6.1f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.GHASTLY,
-                new HandleMaterialStats(0.9f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(0.90f, 0.85f, 1.00f, 1.30f),
                 new HeadMaterialStats(730, 4.7f, 6, 4.5f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.MYSTITE,
-                new HandleMaterialStats(1.05f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(1.05f, 0.80f, 1.00f, 1.25f),
                 new HeadMaterialStats(760, 3.65f, 6, 4.3f),
                 ExtraMaterialStats.DEFAULT);
         addMaterialStats(AdventTiCMaterials.VARSIUM,
-                new HandleMaterialStats(1.0f, 1.0f, 1.0f, 1.0f),
+                new HandleMaterialStats(1.00f, 0.95f, 1.00f, 1.20f),
                 new HeadMaterialStats(870, 5.2f, 3, 3.75f),
                 ExtraMaterialStats.DEFAULT);
     }
